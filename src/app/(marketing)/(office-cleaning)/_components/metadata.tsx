430a7fc2449606f43397ebd35942ec94474351ac
moonGoblin <kaungzaw1996@gmail.com>
2024-03-03 05:25:40 +1100

    updated: metadata

--- src/app/(marketing)/(office-cleaning)/_components/metadata.tsx
+++ src/app/(marketing)/(office-cleaning)/_components/metadata.tsx
@@ -5,8 +5,8 @@ export const getMetadata = (location: string): Metadata => {
     const pathname = getPathname();
 
     return {
-        title: `Commercial Office Cleaning Service In Sydney`,
-        description: `Get a sparkling clean office with our professional office cleaners. Sydney's trusted commercial office cleaning service. Book online in 60 seconds!`,
+        title: `Commercial Office Cleaning Service In Gold Coast`,
+        description: `Get a sparkling clean office with our professional office cleaners. Gold Coast's trusted commercial office cleaning service. Book online in 60 seconds!`,
         alternates: {
             canonical: pathname,
         },
